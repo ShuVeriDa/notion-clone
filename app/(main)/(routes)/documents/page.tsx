dff9482f3bc9520dd76a56f723c53ccf87acbdbc
shuverida <bashtarov@outlook.com>
2024-06-26 14:40:09 +0300

    Added sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+
+interface IDocumentsPageProps {
+}
+
+const DocumentsPage: NextPage<IDocumentsPageProps> = () => {
+  return (
+    <div>
+      DocumentsPage
+    </div>
+  );
+};
+export default DocumentsPage
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import {NextPage} from "next";
+import {ReactNode} from "react";
+import {useConvexAuth} from "convex/react";
+import {Spinner} from "@/components/spinner";
+import {redirect} from "next/navigation";
+import {Navigation} from "@/app/(main)/_components/navigation";
+
+interface IMainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout: NextPage<IMainLayoutProps> = ({children}) => {
+  const {isAuthenticated, isLoading} = useConvexAuth()
+
+  if(isLoading) {
+    return (
+      <div className={"h-full flex items-center justify-center"}>
+        <Spinner size={"lg"} />
+      </div>
+    )
+  }
+  if(!isAuthenticated) {
+    return redirect("/")
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        {children}
+      </main>
+    </div>
+  );
+};
+export default MainLayout
