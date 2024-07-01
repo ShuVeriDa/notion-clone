82cdb2e943b46ebe877d86176f928c58de6e9418
shuverida <bashtarov@outlook.com>
2024-07-01 19:59:20 +0300

    Added user settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,30 @@
+"use client"
+
 import {NextPage} from "next";
+import Image from "next/image";
+import {useUser} from "@clerk/shared/react";
+import {Button} from "@/components/ui/button";
+import {PlusCircle} from "lucide-react";
 
 interface IDocumentsPageProps {
 }
 
 const DocumentsPage: NextPage<IDocumentsPageProps> = () => {
+  const {user} = useUser()
   return (
-    <div>
-      DocumentsPage
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image src={"/consultants.png"}
+             alt={"empty"}
+             height={300}
+             width={300}
+      />
+      <h2 className="text-lg font-medium">
+        Marşa voġiyla {user?.firstName}i Yozan çu
+      </h2>
+      <Button>
+        <PlusCircle className={"h-4 w-4 mr-2"}/>
+        Yoza yazda
+      </Button>
     </div>
   );
 };
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,6 +3,7 @@ import {ChevronsLeft, MenuIcon} from "lucide-react";
 import {useMediaQuery} from "usehooks-ts";
 import {usePathname} from "next/navigation";
 import {cn} from "@/lib/utils";
+import {UserItem} from "@/app/(main)/_components/user-item";
 
 interface INavigationProps {
 }
@@ -109,7 +110,7 @@ export const Navigation: FC<INavigationProps> = () => {
           <ChevronsLeft className={"h-6 w-6"}/>
         </div>
         <div>
-          <p>Action items</p>
+         <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
