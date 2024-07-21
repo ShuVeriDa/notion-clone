20724b5b666243873a4a93d57ba8f67bab7a966a
shuverida <bashtarov@outlook.com>
2024-07-21 10:22:14 +0300

    added toolbar.tsx

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,12 +1,42 @@
+"use client"
+
 import {NextPage} from "next";
+import {Id} from "@/convex/_generated/dataModel";
+import {useQuery} from "convex/react";
+import {api} from "@/convex/_generated/api";
+import {Toolbar} from "@/components/toolbar";
 
 interface IDocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  }
 }
 
-const DocumentIdPage: NextPage<IDocumentIdPageProps> = () => {
+const DocumentIdPage: NextPage<IDocumentIdPageProps> = (
+  {
+    params
+  }
+) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  })
+
+  if (document === undefined) {
+    return <div>
+      Loading...
+    </div>
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
   return (
-    <div>
-      DocumentIdPage
+    <div className={"pb-40"}>
+      <div className={"h-[35vh]"}/>
+      <div className={"md:max-w-3xl lg:max-w-4xl mx-auto"}>
+        <Toolbar initialData={document}/>
+      </div>
     </div>
   );
 };
---
+++ components/icon-picker.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import {FC, ReactNode} from 'react';
+import {useTheme} from "next-themes";
+import EmojiPicker, {EmojiStyle, Theme} from "emoji-picker-react";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+
+interface IIconPickerProps {
+  onChange: (icon: string) => void
+  children?: ReactNode
+  asChild?: boolean
+}
+
+export const IconPicker: FC<IIconPickerProps> = (
+  {
+    asChild,
+    children,
+    onChange
+  }
+) => {
+  const {resolvedTheme} = useTheme()
+  const currentTheme = (resolvedTheme || "light") as keyof typeof themeMap
+
+  const themeMap = {
+    "dark": Theme.DARK,
+    "light": Theme.LIGHT
+  }
+
+  const theme = themeMap[currentTheme]
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild={asChild}>
+        {children}
+      </PopoverTrigger>
+      <PopoverContent className={"p-0 w-full border-none shadow-none"}>
+        <EmojiPicker height={350}
+                     theme={theme}
+                     onEmojiClick={(data) => onChange(data.emoji)}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+};
